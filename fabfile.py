40bb0fa4e3ce5d528c4fe0fb498d3338a76f5562
Yamaguchi Eikichi <megane@glasses-factory.net>
2013-01-31 17:22:52 +0900

    aaaa

--- fabfile.py
+++ fabfile.py
@@ -4,11 +4,9 @@
 from fabric.api import local, run, env, cd, hosts
 
 CODE_DIR = "/var/www/sites/deploytest"
-# env.host = ['192.168.1.24']
-env.user = "megane"
-# env.password = ''
+env.user = "megane"# env.password = ''
 
-# @hosts('localhost:5959')
+@hosts('localhost')
 def commit():
     message = raw_input("Enter a git commit message: ")
     local("git add . && git commit -m \" %s \"" % message)
