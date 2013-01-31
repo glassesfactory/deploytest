b615d53aef660c7183a6a1b3e2376c3067ea9f38
Yamaguchi Eikichi <megane@glasses-factory.net>
2013-01-31 17:56:55 +0900

    doudaro

--- fabfile.py
+++ fabfile.py
@@ -7,22 +7,19 @@ from fabric.context_managers import settings
 CODE_DIR = "/var/www/sites/deploytest"
 
 env.hosts = ['192.168.1.24:22']
-env.user = "megane"
 
-
-@hosts('localhost')
+# @hosts('localhost')
 def commit():
     message = raw_input("Enter a git commit message: ")
     local("git add . && git commit -m \" %s \"" % message)
     local("git push github master")
 
 
-@hosts('192.168.1.24:22')
+# @hosts('192.168.1.24:22')
 def pull():
     with cd(CODE_DIR):
         run("git pull origin master")
         #再読み込み
-
         with settings(warn_only=True):
             run("gaffer unload")
         run("gaffer load")
