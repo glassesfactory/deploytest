5ae9fd5f799859b950a6b54b031e2a99e680080e
Yamaguchi Eikichi <megane@glasses-factory.net>
2013-01-31 17:21:11 +0900

    testtest

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,7 +3,7 @@ bind = 'unix:/tmp/gunicorn/gunicorn.sock'
 preload_app = True
 backlog = 2048
 workers = 1
-worker_class = 'egg:meinheld#gunicorn_worker'
+# worker_class = 'egg:meinheld#gunicorn_worker'
 pidfile = '/tmp/gunicorn/gunicorn.pid'
 
 logfile = '/var/log/gunicorn/gunicorn_dev.log'
